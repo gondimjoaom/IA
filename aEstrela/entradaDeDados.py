550a162f616fc4fb2ca25c0aca0666092b154b75
João Gondim <gondimjoaom@gmail.com>
2019-09-08 04:53:20 -0300

    ainda trabalhando na heuristica

--- aEstrela/entradaDeDados.py
+++ aEstrela/entradaDeDados.py
@@ -1,6 +1,5 @@
 #import classeEstado as node
 import heapq
-import copy
 
 class State (object):
     def __init__(self, pai, profundidade, custo, f, linha_espaco, coluna_espaco, acao, tab):
@@ -43,8 +42,11 @@ class State (object):
             h += abs(x - 1) + abs(y - 2)
         elif self.tab[y][x] == 8:
             h += abs(x - 2) + abs(y - 2)
+        #print(self.custo + h)
         return self.custo + h
         
+    def __lt__(self, other):
+        return self.f < other.f
 
 def isSolution (state):
     if (state[0] == [0, 1, 2]):
@@ -55,8 +57,11 @@ def isSolution (state):
     return False
 
 def printTable(node):
-    for y in node.tab:
-        print(y)
+    if node != 'raiz':
+        for y in node.tab:
+            print(y)
+    else:
+        print('raiz')        
 #MOVIMENTOS
 #para cima:
 
@@ -71,111 +76,172 @@ def expandeNode (node):
             pass
         y += 1
         #vazio = [x, y]
+    
+    
     if x == 0:
         if y == 0: # dois nos criados
-            f = node.heuristica(x, y)
-            newNode1 = State(node, node.profundidade + 1, f, no.custo, x, y, '1 no', node.tab)
-            newNode2 = State(node, node.profundidade + 1, f, no.custo, x, y, '2 no', node.tab)
+            newNode1 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
+            newNode2 = State(node, node.profundidade + 1, node.custo, 0, x, y, '2 no', node.tab)
             newNode1.changeTile(x, y, x + 1, y)
             newNode2.changeTile(x, y, x, y + 1)
-            #printTable(newNode1)
-            #printTable(newNode2)
-            frontier.append(newNode1)
-            frontier.append(newNode2)
+
+            newNode1.f = newNode1.heuristica(x, y) + newNode1.custo
+            newNode2.f = newNode2.heuristica(x, y) + newNode2.custo
+            if (newNode1 not in frontier):
+                heapq.heappush(frontier, newNode1)
+            if (newNode2 not in frontier):
+                heapq.heappush(frontier, newNode2)
+            
 
         elif y == 1:
-            newNode1 = State('ex1', 1, '1', 0, x, y, '1 no', node.tab)
-            newNode2 = State('ex1', 1, '1', 0, x, y, '1 no', node.tab)
-            newNode3 = State('ex1', 1, '1', 0, x, y, '1 no', node.tab)
+            newNode1 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
+            newNode2 = State(node, node.profundidade + 1, node.custo, 0, x, y, '2 no', node.tab)
+            newNode3 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
 
             newNode1.changeTile(x, y, x, y - 1)
             newNode2.changeTile(x, y, x + 1, y)
             newNode3.changeTile(x, y, x, y + 1)
 
-            frontier.append(newNode1)
-            frontier.append(newNode2)
-            frontier.append(newNode3)
+            newNode1.f = newNode1.heuristica(x, y) + newNode1.custo
+            newNode2.f = newNode2.heuristica(x, y) + newNode2.custo
+            newNode3.f = newNode1.heuristica(x, y) + newNode3.custo
+
+            if (newNode1 not in frontier):
+                heapq.heappush(frontier, newNode1)
+            if (newNode2 not in frontier):
+                heapq.heappush(frontier, newNode2)
+            if (newNode3 not in frontier):
+                heapq.heappush(frontier, newNode3)
 
         elif y == 2:
-            newNode1 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode2 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
+            newNode1 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
+            newNode2 = State(node, node.profundidade + 1, node.custo, 0, x, y, '2 no', node.tab)
             
             newNode1.changeTile(x, y, x, y - 1)
             newNode2.changeTile(x, y, x + 1, y)
-            #print('aqui')
-            frontier.append(newNode1)
-            frontier.append(newNode2)
+           
+            newNode1.f = newNode1.heuristica(x, y) + newNode1.custo
+            newNode2.f = newNode2.heuristica(x, y) + newNode2.custo
+            if (newNode1 not in frontier):
+                heapq.heappush(frontier, newNode1)
+            if (newNode2 not in frontier):
+                heapq.heappush(frontier, newNode2)
+
     elif x == 1:
+        
         if y == 0:
-            newNode1 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode2 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode3 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
+            
+            newNode1 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
+            newNode2 = State(node, node.profundidade + 1, node.custo, 0, x, y, '2 no', node.tab)
+            newNode3 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
 
             newNode1.changeTile(x, y, x, y + 1)
             newNode2.changeTile(x, y, x + 1, y)
             newNode3.changeTile(x, y, x - 1, y)
 
-            frontier.append(newNode1)
-            frontier.append(newNode2)
-            frontier.append(newNode3)
+            newNode1.f = newNode1.heuristica(x, y) + newNode1.custo
+            newNode2.f = newNode2.heuristica(x, y) + newNode2.custo
+            newNode3.f = newNode1.heuristica(x, y) + newNode3.custo
+            
+            if (newNode1 not in frontier):
+                heapq.heappush(frontier, newNode1)
+            if (newNode2 not in frontier):
+                heapq.heappush(frontier, newNode2)
+            if (newNode3 not in frontier):
+                heapq.heappush(frontier, newNode3)
         elif y == 1:
-            newNode1 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode2 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode3 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode4 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
+            newNode1 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
+            newNode2 = State(node, node.profundidade + 1, node.custo, 0, x, y, '2 no', node.tab)
+            newNode3 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
+            newNode4 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
 
             newNode1.changeTile(x, y, x, y + 1)
             newNode2.changeTile(x, y, x + 1, y)
-            newNode3.changeTile(x, y, x - 1, y)
-            newNode4.changeTile(x, y, x , y - 1)
+            #newNode3.changeTile(x, y, x - 1, y)
+            #newNode4.changeTile(x, y, x , y - 1)
 
-            frontier.append(newNode1)
-            frontier.append(newNode2)
-            frontier.append(newNode3)
-            frontier.append(newNode4)
+            newNode1.f = newNode1.heuristica(x, y) + newNode1.custo
+            newNode2.f = newNode2.heuristica(x, y) + newNode2.custo
+            newNode3.f = newNode1.heuristica(x, y) + newNode3.custo
+            newNode4.f = newNode1.heuristica(x, y) + newNode4.custo
+
+            if (newNode1 not in frontier):
+                heapq.heappush(frontier, newNode1)
+            if (newNode2 not in frontier):
+                heapq.heappush(frontier, newNode2)
+            if (newNode3 not in frontier):
+                heapq.heappush(frontier, newNode3)
+            if (newNode4 not in frontier):  
+                heapq.heappush(frontier, newNode4)
+
         elif y == 2:
-            newNode1 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode2 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode3 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
+            newNode1 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
+            newNode2 = State(node, node.profundidade + 1, node.custo, 0, x, y, '2 no', node.tab)
+            newNode3 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
             #print('aqui2')
-            newNode1.changeTile(x, y, x, y - 1)
+            #newNode1.changeTile(x, y, x, y - 1)
             newNode2.changeTile(x, y, x + 1, y)
-            newNode3.changeTile(x, y, x - 1, y)
+            #newNode3.changeTile(x, y, x - 1, y)
 
-            frontier.append(newNode1)
-            frontier.append(newNode2)
-            frontier.append(newNode3)
+            newNode1.f = newNode1.heuristica(x, y) + newNode1.custo
+            newNode2.f = newNode2.heuristica(x, y) + newNode2.custo
+            newNode3.f = newNode1.heuristica(x, y) + newNode3.custo
+
+            if (newNode1 not in frontier):
+                heapq.heappush(frontier, newNode1)
+            if (newNode2 not in frontier):
+                heapq.heappush(frontier, newNode2)
+            if (newNode3 not in frontier):
+                heapq.heappush(frontier, newNode3)
     elif x == 2:
         if y == 0:
-            newNode1 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode2 = State('ex2', 1, '1', no.custo, x, y, '2 no', node.tab)
+            newNode1 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
+            newNode2 = State(node, node.profundidade + 1, node.custo, 0, x, y, '2 no', node.tab)
 
             newNode1.changeTile(x, y, x - 1, y)
             newNode2.changeTile(x, y, x, y + 1)
 
-            frontier.append(newNode1)
-            frontier.append(newNode2)
+            newNode1.f = newNode1.heuristica(x, y) + newNode1.custo
+            newNode2.f = newNode2.heuristica(x, y) + newNode2.custo
+
+            if (newNode1 not in frontier):
+                heapq.heappush(frontier, newNode1)
+            if (newNode2 not in frontier):
+                heapq.heappush(frontier, newNode2)
+            
         elif y == 1:
-            newNode1 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode2 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode3 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
+            newNode1 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
+            newNode2 = State(node, node.profundidade + 1, node.custo, 0, x, y, '2 no', node.tab)
+            newNode3 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
 
             newNode1.changeTile(x, y, x, y - 1)
             newNode2.changeTile(x, y, x - 1, y)
             newNode3.changeTile(x, y, x, y + 1)
 
-            frontier.append(newNode1)
-            frontier.append(newNode2)
-            frontier.append(newNode3)
+            newNode1.f = newNode1.heuristica(x, y) + newNode1.custo
+            newNode2.f = newNode2.heuristica(x, y) + newNode2.custo
+            newNode3.f = newNode1.heuristica(x, y) + newNode3.custo
+
+            if (newNode1 not in frontier):
+                heapq.heappush(frontier, newNode1)
+            if (newNode2 not in frontier):
+                heapq.heappush(frontier, newNode2)
+            if (newNode3 not in frontier):
+                heapq.heappush(frontier, newNode3)
         elif y == 2:
-            newNode1 = State('ex1', 1, '1', no.custo, x, y, '1 no', node.tab)
-            newNode2 = State('ex2', 1, '1', no.custo, x, y, '2 no', node.tab)
+            newNode1 = State(node, node.profundidade + 1, node.custo, 0, x, y, '1 no', node.tab)
+            newNode2 = State(node, node.profundidade + 1, node.custo, 0, x, y, '2 no', node.tab)
 
             newNode1.changeTile(x, y, x - 1, y)
             newNode2.changeTile(x, y, x, y - 1)
 
-            frontier.append(newNode1)
-            frontier.append(newNode2)
+            newNode1.f = newNode1.heuristica(x, y) + newNode1.custo
+            newNode2.f = newNode2.heuristica(x, y) + newNode2.custo
+
+            if (newNode1 not in frontier):
+                heapq.heappush(frontier, newNode1)
+            if (newNode2 not in frontier):
+                heapq.heappush(frontier, newNode2)
 
 estado = [[0,0,0],[0,0,0],[0,0,0]]
 print("Digite a configuracao do estado inicial, linha a linha:")
@@ -196,15 +262,22 @@ frontier = []
 no = State('raiz', 0, 0, 0, 0, 0, 'criar', estado)
 no.addTabela(estado)
 
-frontier.append(no)
+#frontier.append(no)
+
+heapq.heappush(frontier, no)
 
 explored = []
 
 while True:
-    no = frontier.pop()
-    estadoAtual = no.tab
-    print(estadoAtual)
-    if isSolution(estadoAtual):
+    #for x in frontier:
+    #    print(x.tab)
+    #    print(x.f)
+    #print('')
+    no = heapq.heappop(frontier)
+    
+    printTable(no)
+    print('')
+    if isSolution(no.tab):
         break
     
     expandeNode(no)
